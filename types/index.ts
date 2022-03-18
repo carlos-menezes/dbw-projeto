32731b3ff1b154f7550887afc432da180b12c293
Carlos Menezes <talk@carlosmenezes.xyz>
2022-03-18 20:51:55 +0000

    remove `RefreshTokenResponse` type

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,3 @@ import LoginRequest from './LoginRequest';
 import LoginResponse from './LoginResponse';
 export type { LoginRequest };
 export type { LoginResponse };
-
-import RefreshTokenResponse from './RefreshTokenResponse';
-export type { RefreshTokenResponse };
