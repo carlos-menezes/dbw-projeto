fe77277128f01fd3981642ad2d6334b72f646c85
Carlos Menezes <talk@carlosmenezes.xyz>
2022-03-18 20:52:06 +0000

    reset index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,5 @@
-import { useContext, useEffect } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-import { api } from '../services/api';
-
 const Index: React.FC = () => {
-  const { user, login } = useContext(AuthContext);
-
-  useEffect(() => {
-    const loginRequest = async () => {
-      login({
-        email: 'carlos-312312',
-        password: '123123'
-      });
-    };
-
-    loginRequest();
-  }, []);
-
-  return <h1>Hello, {user?.firstName}!</h1>;
+  return <></>;
 };
 
 export default Index;
