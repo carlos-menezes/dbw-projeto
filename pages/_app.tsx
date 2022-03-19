7ed86d4a24988676d255191b1155a31764756df8
Carlos Menezes <talk@carlosmenezes.xyz>
2022-03-19 16:52:49 +0000

    Import Carbon Design CSS into the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { AuthProvider } from '../contexts/AuthContext';
 
+import '../styles.scss';
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <AuthProvider>
     <Component {...pageProps} />
