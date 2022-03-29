e79071dea0dd2f651bd9582ad7f6c0a434273f60
Carlos Menezes <talk@carlosmenezes.xyz>
2022-03-29 18:57:32 +0100

    Show comments for everyone

--- pages/ticket/[id].tsx
+++ pages/ticket/[id].tsx
@@ -244,223 +244,237 @@ const TicketId = () => {
       ) : (
         <>
           <Grid>
-            {error && (
+            {error ? (
               <InlineNotification
                 kind={'error'}
                 title={'An error occurred!'}
                 subtitle={error}
               />
-            )}
-            <Row>
-              <Column>
-                <Breadcrumb>
-                  <BreadcrumbItem>Ticket</BreadcrumbItem>
-                  <BreadcrumbItem>{ticketData?.category.title}</BreadcrumbItem>
-                  <BreadcrumbItem>
-                    <Link href={`/tickets/${ticketData?.id}`}>
-                      {ticketData?.id}
-                    </Link>
-                  </BreadcrumbItem>
-                </Breadcrumb>
-              </Column>
-            </Row>
-            <Row>
-              <Column>
-                <h2>{ticketData?.title}</h2>
-              </Column>
-            </Row>
-            <Row>
-              <Column sm={12} md={4} lg={4}>
-                <p>{ticketData?.email}</p>
-              </Column>
-              <Column sm={12} md={4} lg={4}>
-                <p>{new Date(ticketData?.createdAt).toUTCString()}</p>
-              </Column>
-              <Column sm={3} md={3} lg={3}>
-                <Tag
-                  title="Status"
-                  type={statusTagColor[ticketData?.status.toString()]}
-                >
-                  {ticketData?.status}
-                </Tag>
-              </Column>
-            </Row>
-            <Row>
-              <Column>
-                <JustifiedParagraph>
-                  {ticketData?.description}
-                </JustifiedParagraph>
-              </Column>
-            </Row>
-            {isAuthenticated && (
-              <Row>
-                <ActionColumn>
-                  {/* If a reply has been set, an authenticated user is able to create
-                a new FAQ entry from it. */}
-                  {ticketData.messages.filter((m) => m.isReply).length > 0 && (
-                    <Button
-                      kind="secondary"
-                      onClick={(_e) =>
-                        createFAQEntry(
-                          ticketData.title,
-                          ticketData.messages.filter((m) => m.isReply)[0]
-                            .message,
-                          ticketData.categoryId
-                        )
-                      }
-                    >
-                      Create FAQ Entry
-                    </Button>
-                  )}
-                  {ticketData.status === 'AVAILABLE' && (
-                    <Button
-                      kind="primary"
-                      onClick={(_e) => updateTicketStatus('ASSIGN')}
-                    >
-                      Assign
-                    </Button>
-                  )}
-                  {ticketData.status === 'CLOSED' && (
-                    <Button
-                      kind="primary"
-                      onClick={(_e) => updateTicketStatus('OPEN')}
-                    >
-                      OPEN
-                    </Button>
-                  )}
-                  {ticketData.status !== 'CLOSED' && (
-                    <Button
-                      kind="danger--tertiary"
-                      onClick={(_e) => updateTicketStatus('CLOSE')}
-                    >
-                      Close
-                    </Button>
-                  )}
-                </ActionColumn>
-              </Row>
-            )}
-            {faqEntryId && (
+            ) : (
               <>
                 <Row>
                   <Column>
-                    <InlineNotification
-                      kind={'success'}
-                      title={'FAQ Entry Created'}
-                      subtitle={`FAQ entry created with ID ${faqEntryId}.`}
-                    />
+                    <Breadcrumb>
+                      <BreadcrumbItem>Ticket</BreadcrumbItem>
+                      <BreadcrumbItem>
+                        {ticketData?.category.title}
+                      </BreadcrumbItem>
+                      <BreadcrumbItem>
+                        <Link href={`/tickets/${ticketData?.id}`}>
+                          {ticketData?.id}
+                        </Link>
+                      </BreadcrumbItem>
+                    </Breadcrumb>
                   </Column>
                 </Row>
-              </>
-            )}
-            <Divider margin={30} />
-            <CommentsRow>
-              <Column>
-                <h4>Comments ({ticketData?.messages.length})</h4>
-              </Column>
-            </CommentsRow>
-            {!isAuthenticated && !isTicketCreator && (
-              <Form onSubmit={(e) => handleCommentCodeFormSubmission(e)}>
                 <Row>
                   <Column>
-                    <TextInput
-                      invalid={invalidCommentCode}
-                      invalidText={'Invalid comment code. '}
-                      onChange={(e) => setCommentCode(e.target.value)}
-                      id={'code'}
-                      placeholder={'Comment code'}
-                      labelText={''}
-                    />
-                  </Column>
-                  <Column>
-                    <Button type="submit" kind="primary" size="default">
-                      Submit
-                    </Button>
+                    <h2>{ticketData?.title}</h2>
                   </Column>
                 </Row>
-              </Form>
-            )}
-
-            {(isTicketCreator ||
-              (isAuthenticated && ticketData.userId === user.id)) &&
-              ticketData.status !== 'CLOSED' && (
-                <Form onSubmit={handleCommentFormSubmission}>
-                  <Row>
-                    <Column>
-                      <TextArea
-                        onChange={(e) => setComment(e.target.value)}
-                        wrap="true"
-                        labelText={'Comment'}
-                        cols={400}
-                        rows={6}
-                      />
-                    </Column>
-                  </Row>
-                  <Row>
-                    <ActionColumn>
-                      <Button disabled={!comment} type="submit" kind="primary">
-                        Submit
-                      </Button>
-                    </ActionColumn>
-                  </Row>
-                </Form>
-              )}
-
-            {isAuthenticated && ticketData.userId === null && (
-              <Row>
-                <Column>
-                  You must assign this ticket to yourself before commenting.
-                </Column>
-              </Row>
-            )}
-
-            {(isAuthenticated || isTicketCreator) &&
-              ticketData?.messages
-                .sort((a, _b) => (a.isReply ? -1 : 0)) // Pin the reply, if it exists
-                .map((m: TicketMessage & { user: User }) => (
-                  <Column key={m.id} style={{ marginBottom: '20px' }}>
-                    <TicketMessageRow $agent={!!m.userId} $reply={m.isReply}>
-                      <Column>
-                        <p>
-                          {!!m.userId ? (
-                            <>
-                              <span style={{ fontWeight: 700 }}>
-                                {m.user.firstName} {m.user.lastName}
-                              </span>{' '}
-                              ({m.user.email})
-                            </>
-                          ) : (
-                            <>{ticketData.email}</>
-                          )}
-                        </p>
-                        <p>{new Date(m.createdAt).toUTCString()}</p>
-                      </Column>
-                      <Column>
-                        <p>{m.message}</p>
-                      </Column>
-                      {/* Agents should be able to select a reply as the answer to the question. They are also ablet o delete a message. */}
-                      {isAuthenticated && (
-                        <Column>
-                          <ButtonSet>
-                            <Button
-                              kind="tertiary"
-                              onClick={(_e) =>
-                                updateTicketMessageReply(m.id, !m.isReply)
-                              }
-                            >
-                              {m.isReply ? 'Deselect Reply' : 'Select Reply'}
-                            </Button>
-                            <Button
-                              kind="danger--tertiary"
-                              onClick={(_e) => deleteTicketMessage(m.id)}
-                            >
-                              Delete
-                            </Button>
-                          </ButtonSet>
-                        </Column>
-                      )}
-                    </TicketMessageRow>
+                <Row>
+                  <Column sm={12} md={4} lg={4}>
+                    <p>{ticketData?.email}</p>
                   </Column>
-                ))}
+                  <Column sm={12} md={4} lg={4}>
+                    <p>{new Date(ticketData?.createdAt).toUTCString()}</p>
+                  </Column>
+                  <Column sm={3} md={3} lg={3}>
+                    <Tag
+                      title="Status"
+                      type={statusTagColor[ticketData?.status.toString()]}
+                    >
+                      {ticketData?.status}
+                    </Tag>
+                  </Column>
+                </Row>
+                <Row>
+                  <Column>
+                    <JustifiedParagraph>
+                      {ticketData?.description}
+                    </JustifiedParagraph>
+                  </Column>
+                </Row>
+                <>
+                  {isAuthenticated && (
+                    <Row>
+                      <ActionColumn>
+                        {/* If a reply has been set, an authenticated user is able to create
+  a new FAQ entry from it. */}
+                        {ticketData.messages.filter((m) => m.isReply).length >
+                          0 && (
+                          <Button
+                            kind="secondary"
+                            onClick={(_e) =>
+                              createFAQEntry(
+                                ticketData.title,
+                                ticketData.messages.filter((m) => m.isReply)[0]
+                                  .message,
+                                ticketData.categoryId
+                              )
+                            }
+                          >
+                            Create FAQ Entry
+                          </Button>
+                        )}
+                        {ticketData.status === 'AVAILABLE' && (
+                          <Button
+                            kind="primary"
+                            onClick={(_e) => updateTicketStatus('ASSIGN')}
+                          >
+                            Assign
+                          </Button>
+                        )}
+                        {ticketData.status === 'CLOSED' && (
+                          <Button
+                            kind="primary"
+                            onClick={(_e) => updateTicketStatus('OPEN')}
+                          >
+                            OPEN
+                          </Button>
+                        )}
+                        {ticketData.status !== 'CLOSED' && (
+                          <Button
+                            kind="danger--tertiary"
+                            onClick={(_e) => updateTicketStatus('CLOSE')}
+                          >
+                            Close
+                          </Button>
+                        )}
+                      </ActionColumn>
+                    </Row>
+                  )}
+                  {faqEntryId && (
+                    <>
+                      <Row>
+                        <Column>
+                          <InlineNotification
+                            kind={'success'}
+                            title={'FAQ Entry Created'}
+                            subtitle={`FAQ entry created with ID ${faqEntryId}.`}
+                          />
+                        </Column>
+                      </Row>
+                    </>
+                  )}
+                  <Divider margin={30} />
+                  <CommentsRow>
+                    <Column>
+                      <h4>Comments ({ticketData?.messages.length})</h4>
+                    </Column>
+                  </CommentsRow>
+                  {!isAuthenticated && !isTicketCreator && (
+                    <Form onSubmit={(e) => handleCommentCodeFormSubmission(e)}>
+                      <Row>
+                        <Column>
+                          <TextInput
+                            invalid={invalidCommentCode}
+                            invalidText={'Invalid comment code. '}
+                            onChange={(e) => setCommentCode(e.target.value)}
+                            id={'code'}
+                            placeholder={'Comment code'}
+                            labelText={''}
+                          />
+                        </Column>
+                        <Column>
+                          <Button type="submit" kind="primary" size="default">
+                            Submit
+                          </Button>
+                        </Column>
+                      </Row>
+                    </Form>
+                  )}
+                  {(isTicketCreator ||
+                    (isAuthenticated && ticketData.userId === user.id)) &&
+                    ticketData.status !== 'CLOSED' && (
+                      <Form onSubmit={handleCommentFormSubmission}>
+                        <Row>
+                          <Column>
+                            <TextArea
+                              onChange={(e) => setComment(e.target.value)}
+                              wrap="true"
+                              labelText={'Comment'}
+                              cols={400}
+                              rows={6}
+                            />
+                          </Column>
+                        </Row>
+                        <Row>
+                          <ActionColumn>
+                            <Button
+                              disabled={!comment}
+                              type="submit"
+                              kind="primary"
+                            >
+                              Submit
+                            </Button>
+                          </ActionColumn>
+                        </Row>
+                      </Form>
+                    )}
+                  {isAuthenticated && ticketData.userId === null && (
+                    <Row>
+                      <Column>
+                        You must assign this ticket to yourself before
+                        commenting.
+                      </Column>
+                    </Row>
+                  )}
+                  {ticketData?.messages
+                    .sort((a, _b) => (a.isReply ? -1 : 0)) // Pin the reply, if it exists
+                    .map((m: TicketMessage & { user: User }) => (
+                      <Column key={m.id} style={{ marginBottom: '20px' }}>
+                        <TicketMessageRow
+                          $agent={!!m.userId}
+                          $reply={m.isReply}
+                        >
+                          <Column>
+                            <p>
+                              {!!m.userId ? (
+                                <>
+                                  <span style={{ fontWeight: 700 }}>
+                                    {m.user.firstName} {m.user.lastName}
+                                  </span>{' '}
+                                  ({m.user.email})
+                                </>
+                              ) : (
+                                <>{ticketData.email}</>
+                              )}
+                            </p>
+                            <p>{new Date(m.createdAt).toUTCString()}</p>
+                          </Column>
+                          <Column>
+                            <p>{m.message}</p>
+                          </Column>
+                          {/* Agents should be able to select a reply as the answer to the question. They are also ablet o delete a message. */}
+                          {isAuthenticated && (
+                            <Column>
+                              <ButtonSet>
+                                <Button
+                                  kind="tertiary"
+                                  onClick={(_e) =>
+                                    updateTicketMessageReply(m.id, !m.isReply)
+                                  }
+                                >
+                                  {m.isReply
+                                    ? 'Deselect Reply'
+                                    : 'Select Reply'}
+                                </Button>
+                                <Button
+                                  kind="danger--tertiary"
+                                  onClick={(_e) => deleteTicketMessage(m.id)}
+                                >
+                                  Delete
+                                </Button>
+                              </ButtonSet>
+                            </Column>
+                          )}
+                        </TicketMessageRow>
+                      </Column>
+                    ))}
+                </>
+              </>
+            )}
           </Grid>
         </>
       )}
