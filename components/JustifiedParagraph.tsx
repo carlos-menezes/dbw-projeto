eaca715f8c9a20f5d1ae9038500b75bf2e194de3
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-04-08 13:48:50 +0100

    Cleanup `JustifiedParagraph`

--- components/JustifiedParagraph.tsx
+++ components/JustifiedParagraph.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const JustifiedParagraph = styled.p`
   text-align: justify;
   overflow-wrap: break-word;
-  /* white-space: pre-wrap; */
 `;
 
 export default JustifiedParagraph;
