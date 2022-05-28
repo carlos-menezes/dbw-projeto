f8966fb1ef5acc67abfc9f581baf5f26061f758a
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-05-28 14:49:56 +0100

    Fix available ticket list

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -323,7 +323,7 @@ const Dashboard: React.FC<DashboardProps> = ({
                 <CustomTable
                   title={'Available Tickets'}
                   tickets={tickets}
-                  ticketStatus={'CLOSED'}
+                  ticketStatus={'AVAILABLE'}
                 />
 
                 <Divider margin={15} />
